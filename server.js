a0ed4b784e23ed1cffa92e44bf91f64527b35fbd
lucaslmota <lucaslmota0431@gmail.com>
2022-09-15 14:02:21 -0300

    vai dar bom1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get(["/", "/:name"], (req, res) => {
-  greeting = "<h1>Hello From Node on Fly lucas teste!</h1>";
+  greeting = "<h1>Hello From Node on Fly lucas teste agora vai!</h1>";
   name = req.params["name"];
   if (name) {
     res.send(greeting + "</br>and hello to " + name);
